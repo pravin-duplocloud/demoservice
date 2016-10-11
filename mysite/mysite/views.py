cff95710f555d1f6c8ef7b2d66bd04650898f964
Venketesan <thvenket@gmail.com>
2016-10-11 14:50:41 +0000

    Add Missing files

---
+++ mysite/mysite/views.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+# Put Standard Library Imports Here:
+import json
+import traceback
+
+# Put Third Party/Django Imports Here:
+# from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_POST
+
+@require_GET
+def getInfo(httpRequest):
+	response = 'CICD DEMO LETS SEE IF IT WORKS!'
+	return HttpResponse(response, content_type="application/json")
+
