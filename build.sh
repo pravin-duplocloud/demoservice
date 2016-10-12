15d88a08ae8bdb64c647c1cfc92ad25a8c18ff2c
duplodemo <venkat@duplocloud.net>
2016-10-12 01:35:00 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -e
-sleep 1h
 docker info
 docker build -t djangoservice .
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker tag -f djangoservice duplodemo/anyservice:$CIRCLE_SHA1
+docker tag -f djangoservice duplocloud/anyservice:$CIRCLE_SHA1
 docker push duplodemo/anyservice:$CIRCLE_SHA1
 
 echo "Docker Build Finished.."
