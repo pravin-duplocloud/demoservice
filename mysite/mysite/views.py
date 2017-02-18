6ecfe081e0758be0fa44922b5fcd9c5c37c72d54
Venketesan <thvenket@gmail.com>
2017-02-18 12:38:16 -0800

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'CICD DEMO LETS SEE IF IT WORKS!'
+	response = 'CICD DEMO!'
 	return HttpResponse(response, content_type="application/json")
 
