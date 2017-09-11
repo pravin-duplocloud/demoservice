99ceed7fd277e2c2ed8d729c2e53612376a98d59
duplodemo <venkat@duplocloud.net>
2017-09-10 18:32:07 -0700

    sinday

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'CICD DEMO!'
+	response = 'CICD DEMO today sunday!'
 	return HttpResponse(response, content_type="application/json")
 
