e5247054966f55eaee5d05bd55185b6ea98fb953
Venketesan <venkat@duplocloud.net>
2017-11-10 12:52:42 -0800

    vara demo

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'CICD DEMO!'
+	response = 'Vara demo'
 	return HttpResponse(response, content_type="application/json")
 
