9835d86caf885fc33e5222f09a5b18fcc498ba59
venkat <venkat@duplocloud.net>
2017-11-12 22:30:36 -0800

    updates

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -14,5 +14,5 @@ from django.views.decorators.http import require_POST
 @require_GET
 def getInfo(httpRequest):
 	response = 'CICD DEMO!'
-	return HttpResponse(response, content_type="application/json")
+	return HttpResponse(response, content_type="text/html")
 
