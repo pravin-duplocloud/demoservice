f58b1452e7553143556b83a6e741b8eab078d57d
Venketesan <venkat@duplocloud.net>
2017-11-28 18:07:54 -0800

    test me
    shshs

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'CICD DEMO!'
+	response = 'dgdhdtdy DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
