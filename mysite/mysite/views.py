afc22cd75927861b8067a357a51980adf88a33de
Venketesan <venkat@duplocloud.net>
2017-12-13 23:56:07 -0800

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -15,6 +15,15 @@ from django.views.decorators.http import require_POST
 @require_GET
 def getInfo(httpRequest):
         expr = httpRequest.GET.get('expr')
+        print 'Client IP ' + get_client_ip(httpRequest) + ' expression ' + expr 
         response = eval(expr)
         return HttpResponse(response, content_type="text/html")
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
