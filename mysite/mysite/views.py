57c6ae31ecdc28184658d30f04f7d3a6d4014e4c
Venketesan <venkat@duplocloud.net>
2018-11-08 14:36:28 -0800

    feature test

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'ICIX CICD DEMO!'
+	response = 'ICIX CICD DEMO for feature branch!'
 	return HttpResponse(response, content_type="text/html")
 
