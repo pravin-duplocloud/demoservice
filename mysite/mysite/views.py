4874ae9ed861bf3d0d10c38a6b08b0d21a54ed84
duplocloud <venkthi@gmail.com>
2019-05-17 09:43:35 -0700

    Update views.py

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'ICIX CICD DEMO!'
+	response = 'CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
