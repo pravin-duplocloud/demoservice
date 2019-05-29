befc80267a97648216d4be7aaa99afef11a93b78
Venketesan <venkat@duplocloud.net>
2019-05-29 09:59:01 -0700

    Amit demo

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 't98 CICD DEMO!'
+	response = 'Amit CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
