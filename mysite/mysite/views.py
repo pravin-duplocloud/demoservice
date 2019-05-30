1298ae6c1ed97028d0bb2c86fc0d325894385ba9
Venketesan <venkat@duplocloud.net>
2019-05-30 13:13:17 -0700

    kp

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'Amit CICD DEMO!'
+	response = 'KP CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
