b4945542b4ee513045867ba0c2b4279976e554df
Venketesan <venkat@duplocloud.net>
2019-06-26 08:18:42 -0700

    web765

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'SCB01 CICD DEMO!'
+	response = 'WEB765 CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
