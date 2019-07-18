dae5e49f3959771176fefdda70bb32d7cbca2446
Venketesan <venkat@duplocloud.net>
2019-07-18 09:56:33 -0700

    demo pull request

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'WEB765 CICD DEMO!'
+	response = 'Skyone demo CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
