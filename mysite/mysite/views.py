6fe79c4099e776776eb9a6bfca4ade50febe5554
Venketesan <venkat@duplocloud.net>
2019-08-21 04:23:27 -0700

    test demo xdfrt

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'rupee CICD DEMO!'
+	response = 'sxcvfrt CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
