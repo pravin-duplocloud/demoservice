57d57ad5f3811e93253c2092397a9ba0e702b80e
Venketesan <venkat@duplocloud.net>
2019-09-09 09:51:58 -0700

    drupal code

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'ertyrt CICD DEMO!'
+	response = 'drupal CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
