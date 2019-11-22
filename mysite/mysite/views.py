726cf374dd7b7873725a6dbead0db822b35c95e9
Venketesan <venkat@duplocloud.net>
2019-11-21 23:06:04 -0800

    demo nnn

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'john CICD DEMO!'
+	response = 'ffkhfk CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
