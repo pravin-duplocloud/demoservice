1d64e7bd9e119c2f0efc4a403e91c0e6a0d2b42e
Venketesan <venkat@duplocloud.net>
2019-12-13 14:41:18 -0800

    new deployment

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'megazone CICD DEMO!'
+	response = 'nghtyiis CICD DEMO!'
 	return HttpResponse(response, content_type="text/html")
 
