a239071c531009af2e42d68dffef00c9fda1fe56
Venketesan <venkat@duplocloud.net>
2020-01-19 03:25:14 -0800

    Demo

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'ghtyi9686896 CICD DEMO!'
+	response = 'DuploCloud deep Dive CICD Demo!'
 	return HttpResponse(response, content_type="text/html")
 
