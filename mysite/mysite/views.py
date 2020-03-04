b7b1bab206c2173f085050223c9cd3594c3ab18f
duplocloud <venkthi@gmail.com>
2020-03-04 01:36:55 -0800

    CICD Demo here!

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'Today lets do a CICD demo!'
+	response = 'Doing a CICD demo again!'
 	return HttpResponse(response, content_type="text/html")
 
