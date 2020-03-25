ac824fb52c484c8a9f29cffecb49f5e0377a1064
Venketesan <venkat@duplocloud.net>
2020-03-25 06:56:30 -0700

    demo change

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'Welcome to the Payment Hub application!'
+	response = 'Welcome to the Payment Husshshs !'
 	return HttpResponse(response, content_type="text/html")
 
