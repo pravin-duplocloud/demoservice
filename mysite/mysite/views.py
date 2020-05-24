ab5ea37fa1440287f98d2d767aa664b464b07ed4
Venketesan <venkat@duplocloud.net>
2020-05-24 11:36:29 -0700

    demo commit

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'my demo today !'
+	response = 'my demo today at sunday may 24 !'
 	return HttpResponse(response, content_type="text/html")
 
