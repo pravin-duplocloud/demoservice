0c17e18fb1bb9499408ad16d85f7b41662696179
Venketesan <venkat@duplocloud.net>
2020-06-18 23:55:08 -0700

    biginfodemo

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'my demo of build as a microservice !'
+	response = 'my demo of build as a biginfo !'
 	return HttpResponse(response, content_type="text/html")
 
