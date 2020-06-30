5b5595ce3176528c8b4f824ed7c2d268b074bfe0
pravin <57933252+pravin-duplocloud@users.noreply.github.com>
2020-06-29 22:45:02 -0700

    test cicd .

--- mysite/lambda_update.py
+++ mysite/lambda_update.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
 import os
 import sys
+import json
 import requests
 
 def update_lambda():
 
+    s3_file = 'mysite-zappa.zip'
     API_TOKEN = os.getenv('DUPLO_API_TOKEN')
     S3_BUCKET_LAMBDA = os.getenv('S3_BUCKET_LAMBDA')
     TENANTID = os.getenv('TENANTID')
-    CI_TENANT_NAME = os.getenv('CI_TENANT_NAME')
     DUPLO_URL = os.getenv('DUPLO_URL')
-    s3_file = 'mysite-zappa.zip'
 
-    #UpdateLambdaFunctionConfiguration
-    headers = {"Authorization": "Bearer {0}".format( API_TOKEN ),
-                "Content-Type":"application/json; charset=utf-8",
-                "Accept":"application/json, text/plain, */*"}
-    request = {}
-    request["FunctionName"]="duploservices-dev01-helloworld-128329325849"
-    request["Timeout"]=20
-    request["MemorySize"]=128
-    request["Handler"]="handler.lambda_handler"
-    request["Description"]="api gateway demo"
-    request["Runtime"]="python3.7"
-    print("UpdateLambdaFunctionConfiguration start ", request)
-    #https://msp.duplocloud.net/subscriptions/3369e68e-2bf7-415e-b78b-6cb6aef327ec/UpdateLambdaFunctionConfiguration
+    headers = {
+        "Authorization": "Bearer {0}".format( API_TOKEN ),
+                'Content-Type': 'application/json'
+        }
+    data = {
+        "FunctionName": "duploservices-dev01-helloworld-128329325849",
+        "Timeout": 20,
+        "MemorySize":128,
+        "Handler":"handler.lambda_handler",
+        "Description":"api gateway demo",
+        "Runtime":"python3.6"
+    }
+
+    data = json.dumps(data)
+    print("UpdateLambdaFunctionConfiguration start ", data)
     endpoint = "{0}/subscriptions/{1}/UpdateLambdaFunctionConfiguration".format(DUPLO_URL, TENANTID)
-    response = requests.post(endpoint, headers=headers , data=request)
+    response = requests.post(endpoint, headers=headers , data=data)
     print("UpdateLambdaFunctionConfiguration response ", endpoint, response)
 
     #UpdateLambdaFunction
-    request = {}
-    request["FunctionName"]="duploservices-dev01-helloworld-128329325849"
-    request["S3Bucket"]=S3_BUCKET_LAMBDA
-    request["S3Key"]=s3_file
-    print("UpdateLambdaFunction start ", request)
+    data = {
+     "FunctionName":"duploservices-dev01-helloworld-128329325849",
+     "S3Bucket":S3_BUCKET_LAMBDA,
+     "S3Key":s3_file
+    }
+    data = json.dumps(data)
+    print("UpdateLambdaFunction start ", data)
     endpoint = "{0}/subscriptions/{1}/UpdateLambdaFunction".format(DUPLO_URL, TENANTID)
-    response = requests.post(endpoint, headers=headers , data=request)
+    response = requests.post(endpoint, headers=headers , data=data)
     print("UpdateLambdaFunction response ", endpoint, response)
 
 if __name__ == "__main__":
