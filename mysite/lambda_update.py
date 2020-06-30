0f4f853223662f95332e22abd009ddb1ff727e66
pravin <57933252+pravin-duplocloud@users.noreply.github.com>
2020-06-29 21:24:31 -0700

    test cicd .

--- mysite/lambda_update.py
+++ mysite/lambda_update.py
@@ -11,26 +11,33 @@ def update_lambda():
     CI_TENANT_NAME = os.getenv('CI_TENANT_NAME')
     DUPLO_URL = os.getenv('DUPLO_URL')
     s3_file = 'mysite-zappa.zip'
+
+    #UpdateLambdaFunctionConfiguration
+
+    request = {}
+    request["FunctionName"]="duploservices-dev01-helloworld-128329325849"
+    request["Timeout"]=20
+    request["MemorySize"]=128
+    request["Handler"]="handler.lambda_handler"
+    request["Description"]="api gateway demo"
+    request["Runtime"]="python3.7"
+    print("UpdateLambdaFunctionConfiguration start ", request)
+    #https://msp.duplocloud.net/subscriptions/3369e68e-2bf7-415e-b78b-6cb6aef327ec/UpdateLambdaFunctionConfiguration
+    endpoint = "{0}/subscriptions/{1}/UpdateLambdaFunctionConfiguration".format(DUPLO_URL, TENANTID)
+    headers = {"Authorization": "Bearer {0}".format( API_TOKEN )}
+    response = requests.post(endpoint, headers=headers , data=request)
+    print("UpdateLambdaFunctionConfiguration response ", response)
+
+    #UpdateLambdaFunction
     request = {}
     request["FunctionName"]="duploservices-dev01-helloworld-128329325849"
     request["S3Bucket"]=S3_BUCKET_LAMBDA
     request["S3Key"]=s3_file
-
-    # UpdateLambdaFunctionConfiguration
-    # request["Timeout"]=20
-    # request["MemorySize"]=128
-    # request["Handler"]="handler.lambda_handler"
-    # request["Description"]="api gateway demo"
-    # request["Runtime"]="python3.7"
-
-    #https://msp.duplocloud.net/subscriptions/3369e68e-2bf7-415e-b78b-6cb6aef327ec/UpdateLambdaFunctionConfiguration
-    # endpoint = "{0}/subscriptions/{1}/UpdateLambdaFunctionConfiguration".format(DUPLO_URL, TENANTID)
+    print("UpdateLambdaFunction start ", request)
     endpoint = "{0}/subscriptions/{1}/UpdateLambdaFunction".format(DUPLO_URL, TENANTID)
     headers = {"Authorization": "Bearer {0}".format( API_TOKEN )}
     response = requests.post(endpoint, headers=headers , data=request)
-    print(response)
+    print("UpdateLambdaFunction response ", response)
 
 if __name__ == "__main__":
     update_lambda()
-
- 
