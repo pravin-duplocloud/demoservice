e54a1fe54f87827f2cf40252eb89ee1ee2817629
pravin <57933252+pravin-duplocloud@users.noreply.github.com>
2020-06-29 17:44:43 -0700

    test cicd .

--- ci.sh
+++ ci.sh
@@ -2,10 +2,15 @@
 
 if [[ "x$PHASE" != "x" ]]; then
   if [[ "$PHASE" == "PRE_DEPLOY_BUILD" ]]; then
-    echo "Demo Test Build Docker image and push to docker hub!"
-    export S3_BUCKET_LAMBDA=$S3_BUCKET_LAMBDA
-    ./build-zappa.sh
-    exit 0
+    if [[ "x$S3_BUCKET_LAMBDA" != "x" ]]; then
+      echo "Demo Test Build Docker image and push to docker hub!"
+      export S3_BUCKET_LAMBDA="$S3_BUCKET_LAMBDA"
+      ./build-zappa.sh
+      exit 0
+    else
+      echo "S3_BUCKET_LAMBDA parameter is not provided"
+      exit 1
+    fi
   elif [[ "$PHASE" == "POST_DEPLOY_VERIFICATION" ]]; then
     echo "No  action needed, Run under tests section in cci"
     exit 0
