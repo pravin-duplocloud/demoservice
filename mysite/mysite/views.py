caf8b16c8160ddf1d1e71b8658bf5a46058ce3d8
pravin <57933252+pravin-duplocloud@users.noreply.github.com>
2020-06-29 21:08:25 -0700

    test cicd .

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -13,6 +13,6 @@ from django.views.decorators.http import require_POST
 
 @require_GET
 def getInfo(httpRequest):
-	response = 'my demo of build for iot prject !'
-	return HttpResponse(response, content_type="text/html")
+	response = 'my demo of build for iot prject ! demo !'
 
+	return HttpResponse(response, content_type="text/html")
