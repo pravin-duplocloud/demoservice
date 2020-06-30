94bbbe1405ddef516315468d379bfc200a61a885
pravin <57933252+pravin-duplocloud@users.noreply.github.com>
2020-06-29 19:58:08 -0700

    test cicd .

--- katkitconfig.js
+++ katkitconfig.js
@@ -12,33 +12,5 @@
         "ContainerImage": "duplocloud/zbuilder:lambda_v11"
       }
     ]
-  },
-  {
-    "EnvName": "dev",
-    "LocalFleet": "true",
-    "WorkFlow": [
-			{
-        "Name": "PRE_DEPLOY_BUILD",
-        "PhaseType": 4,
-        "BuildParams": "PHASE=PRE_DEPLOY_BUILD, FOO=BAR1",
-        "Order": 0,
-        "Parallelism": 1,
-        "ContainerImage": "duplocloud/zbuilder:lambda_v11"
-      }
-    ]
-  },
-  {
-    "EnvName": "prod",
-    "LocalFleet": "true",
-    "WorkFlow": [
-			{
-				"Name": "PRE_DEPLOY_BUILD",
-				"PhaseType": 4,
-				"BuildParams": "PHASE=PRE_DEPLOY_BUILD, FOO=BAR1",
-				"Order": 0,
-				"Parallelism": 1,
-				"ContainerImage": "duplocloud/zbuilder:lambda_v11"
-			}
-    ]
-  }
+  } 
 ]
