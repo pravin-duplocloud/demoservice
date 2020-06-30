90a92640bdfa171bdf5c8a67475a91c4557eb087
pravin <57933252+pravin-duplocloud@users.noreply.github.com>
2020-06-29 18:42:57 -0700

    test cicd .

--- katkitconfig.js
+++ katkitconfig.js
@@ -9,7 +9,7 @@
 						  "BuildParams":"PHASE=PRE_DEPLOY_BUILD, FOO=BAR1",
 						  "Order":0,
 						  "Parallelism":1,
-						  "ContainerImage":"duplocloud/zbuilder:lambda_v7"
+						  "ContainerImage":"duplocloud/zbuilder:lambda_v8"
 					   },
 					   {
 						  "Name":"DEPLOY",
@@ -17,7 +17,7 @@
 						  "BuildParams":"PHASE=DEPLOY",
 						  "Order":1,
 						  "Parallelism":1,
-						  "ContainerImage":"duplocloud/zbuilder:lambda_v7"
+						  "ContainerImage":"duplocloud/zbuilder:lambda_v8"
 					   },
 			                   {
 						  "Name":"POST_DEPLOY_VERIFICATION",
@@ -25,7 +25,7 @@
 						  "BuildParams":"PHASE=POST_DEPLOY_VERIFICATION, FOO=BAR",
 						  "Order":2,
 						  "Parallelism":1,
-						  "ContainerImage":"duplocloud/zbuilder:lambda_v7"
+						  "ContainerImage":"duplocloud/zbuilder:lambda_v8"
 					   }
 					]
 	  },
@@ -39,7 +39,7 @@
 						  "BuildParams":"PHASE=PRE_DEPLOY_BUILD, FOO=BAR",
 						  "Order":0,
 						  "Parallelism":1,
-						  "ContainerImage":"duplocloud/zbuilder:lambda_v7"
+						  "ContainerImage":"duplocloud/zbuilder:lambda_v8"
 					   },
 					   {
 						  "Name":"DEPLOY",
@@ -47,7 +47,7 @@
 						  "BuildParams":"PHASE=DEPLOY",
 						  "Order":1,
 						  "Parallelism":1,
-						  "ContainerImage":"duplocloud/zbuilder:lambda_v7"
+						  "ContainerImage":"duplocloud/zbuilder:lambda_v8"
 					   }
 					]
 	  },
@@ -61,7 +61,7 @@
 						  "BuildParams":"PHASE=DEPLOY",
 						  "Order":1,
 						  "Parallelism":1,
-						  "ContainerImage":"duplocloud/zbuilder:lambda_v7"
+						  "ContainerImage":"duplocloud/zbuilder:lambda_v8"
 					   },
 			                   {
 						  "Name":"POST_DEPLOY_VERIFICATION",
@@ -69,7 +69,7 @@
 						  "BuildParams":"PHASE=POST_DEPLOY_VERIFICATION, FOO=BAR",
 						  "Order":2,
 						  "Parallelism":1,
-						  "ContainerImage":"duplocloud/zbuilder:lambda_v7"
+						  "ContainerImage":"duplocloud/zbuilder:lambda_v8"
 					   }
 					]
 	  }
